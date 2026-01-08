29821039cb07e99b9444f951c5060a13662849c8
Harinandini Bandreddi <harinandini@ngenux.com>
2026-01-08 16:39:27 +0530

    removed keyboard avoider package

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -1,5 +1,4 @@
 import { SocketProvider } from "@/providers/WebSocketProvider";
-import { KeyboardAvoiderProvider } from "@good-react-native/keyboard-avoider";
 import { Stack } from "expo-router";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -7,23 +6,21 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 function ProtectedLayout() {
   return (
     <SocketProvider>
-      <KeyboardAvoiderProvider>
-        <SafeAreaProvider>
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="new-chat"
-              options={{
-                title: "Select Contact",
-              }}
-            />
-            <Stack.Screen name="chat/[uuid]" />
-            <Stack.Screen name="(top-tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-            <Stack.Screen name="camera" options={{ headerShown: false }} />
-          </Stack>
-        </SafeAreaProvider>
-      </KeyboardAvoiderProvider>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="new-chat"
+            options={{
+              title: "Select Contact",
+            }}
+          />
+          <Stack.Screen name="chat/[uuid]" />
+          <Stack.Screen name="(top-tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+          <Stack.Screen name="camera" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaProvider>
     </SocketProvider>
   );
 }
--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -1,5 +1,4 @@
 import { FontAwesome6, Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { KeyboardAvoiderView } from "@good-react-native/keyboard-avoider";
 import * as ImagePicker from "expo-image-picker";
 import { useRouter } from "expo-router";
 import React from "react";
@@ -47,48 +46,43 @@ function ChatInput({
   };
 
   return (
-    <KeyboardAvoiderView avoidMode="whole-view" extraSpace={0}>
-      <SafeAreaView edges={["bottom"]}>
-        <View style={styles.inputBar}>
+    <SafeAreaView edges={["bottom"]}>
+      <View style={styles.inputBar}>
+        <TouchableOpacity
+          style={styles.iconButton}
+          onPress={() => setShowEmojiPicker(true)}
+        >
+          <FontAwesome6 name="face-smile" size={24} color="#667781" />
+        </TouchableOpacity>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Message"
+            placeholderTextColor="#8696A0"
+            value={message}
+            onChangeText={setMessage}
+            multiline
+          />
           <TouchableOpacity
-            style={styles.iconButton}
-            onPress={() => setShowEmojiPicker(true)}
+            style={styles.attachButton}
+            onPress={handleLoadFromGallery}
           >
-            <FontAwesome6 name="face-smile" size={24} color="#667781" />
+            <MaterialIcons name="attachment" size={24} color="#667781" />
           </TouchableOpacity>
-
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Message"
-              placeholderTextColor="#8696A0"
-              value={message}
-              onChangeText={setMessage}
-              multiline
-            />
-            <TouchableOpacity
-              style={styles.attachButton}
-              onPress={handleLoadFromGallery}
-            >
-              <MaterialIcons name="attachment" size={24} color="#667781" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.attachButton}
-              onPress={handleCamera}
-            >
-              <Ionicons name="camera" size={22} color="#667781" />
-            </TouchableOpacity>
-          </View>
-
-          <TouchableOpacity
-            style={styles.sendButton}
-            onPress={() => sendMessage()}
-          >
-            <MaterialIcons name="send" size={24} color="#FFFFFF" />
+          <TouchableOpacity style={styles.attachButton} onPress={handleCamera}>
+            <Ionicons name="camera" size={22} color="#667781" />
           </TouchableOpacity>
         </View>
-      </SafeAreaView>
-    </KeyboardAvoiderView>
+
+        <TouchableOpacity
+          style={styles.sendButton}
+          onPress={() => sendMessage()}
+        >
+          <MaterialIcons name="send" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
